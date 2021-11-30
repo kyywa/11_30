8c87ecdc8262605b1d24c7aecaf7f247ec915e24
Tanuló 301-18 <301-18@KKSZKI.LOCAL>
2021-11-30 18:16:19 +0100

    2. Feladat kész

--- orai_munka.py
+++ orai_munka.py
@@ -1,2 +1,6 @@
+#Jancsurák Bence
+
 for i in range(1,10):
     print("{} -- {}".format(i,1/i))
+
+print("Az eredmény: {}".format(int(input("Adja meg a hatvány értékét: "))**int(input("Adja meg a hatvány kitevőjét: "))))

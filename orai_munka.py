4ba4fd83092aef134909b6719e63fd47574ff164
Tanuló 301-18 <301-18@KKSZKI.LOCAL>
2021-11-30 18:19:56 +0100

    3. Feladat kész

--- orai_munka.py
+++ orai_munka.py
@@ -4,3 +4,11 @@ for i in range(1,10):
     print("{} -- {}".format(i,1/i))
 
 print("Az eredmény: {}".format(int(input("Adja meg a hatvány értékét: "))**int(input("Adja meg a hatvány kitevőjét: "))))
+
+while True:
+    num = int(input("Írjon be egy számot: "))
+    if num<0:
+        print("Knock it off with them negative waves")
+    else:
+        print("Siker!")
+        break
